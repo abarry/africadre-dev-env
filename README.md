a715530d2add4f94e1ae2906b3342ea5459e6995
Abdoulaye BARRY <contact@abdoulayebarry.com>
2017-06-18 23:39:54 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,20 @@
+Africadre dev env
+=================
+
+Requirements
+----
+- vagrant + shell + virtualbox
+- ubuntu/xenial64
+- php7
+- nginx
+- mysql 5.7
+
+How to install ?
+--------------
+In the host machine, ensure vagrant and virtualbox are installed
+
+```shell
+$ git clone git@github.com:abarry/africadre-dev-env.git africadre-dev-env
+$ cd africadre-dev-env
+$ vagrant up --provision
+```
---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04
+
+sudo apt-get update
+sudo apt-get install -y nginx php-fpm php-mysql mysql-server php7.0-intl php7.0-xml php7.0-json php7.0-zip
+
+#configure php
+sudo sed -i 's#;date.timezone =#date.timezone = "Europe/Paris"#' /etc/php/7.0/fpm/php.ini
+sudo sed -i 's#;date.timezone =#date.timezone = "Europe/Paris"#' /etc/php/7.0/cli/php.ini
+
+sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+sudo chmod a+x /usr/local/bin/symfony
+
+# Install composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+sudo service php7.0-fpm restart
+sudo service nginx restart
